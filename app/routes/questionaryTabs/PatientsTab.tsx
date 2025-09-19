9744681f25ed97d0be4e7eff469b9d0e8415eb64
Perki <perki@pryv.com>
2025-09-19 13:39:56 +0200

    Now creating invites

--- app/routes/questionaryTabs/PatientsTab.tsx
+++ app/routes/questionaryTabs/PatientsTab.tsx
@@ -2,12 +2,43 @@ import { useTranslation } from 'react-i18next';
 import { PatientsTable } from '@/components/PatientsTable';
 import { QuestionnaryLayout } from '@/routes/layouts/QuestionnaryLayout';
 import type Collector from 'hds-lib-js/types/appTemplates/Collector';
+import { useState } from 'react';
 
 export default function PatientsTab() {
   const { t } = useTranslation();
+ 
+  const [patientRef, setPatientRef] = useState('');
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+  function showErrorMessage(message: string) {
+    const patientRefInput = document.getElementById('patientRef') as HTMLObjectElement;
+    patientRefInput!.setCustomValidity(message);
+  }
+
+  async function _createSharingLink(collector: Collector) {
+    try {
+      console.log('## Clicked >>', patientRef);
+      if (patientRef.length < 5) {
+        throw new Error('Sharing title too short (4 char min)');
+      }
+      const options = { customData: { hello: 'bob' } };
+      const invite = await collector.createInvite(patientRef, options);
+      const inviteSharingData = await invite.getSharingData();
+      console.log('## createInvite newInvite and sharing', { invite, inviteSharingData });
+    } catch (e: any) {
+      showErrorMessage(e.message);
+    }
+    setButtonDisabled(false);
+  }
+
   return (
     <QuestionnaryLayout
       render={(collector: Collector) => {
+        const createSharingLink = async () => { // used to pass "collector"
+          setButtonDisabled(true);
+          _createSharingLink(collector);
+          return false;
+        };
         return (
           <>
             <article className="my-2 prose">
@@ -23,14 +54,16 @@ export default function PatientsTab() {
               <div className="relative w-1/2">
                 <input
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                  id="search"
+                  id="patientRef"
+                  onInput={e => setPatientRef((e.target as HTMLTextAreaElement).value)}
                   placeholder={t('createSharingLinkPlaceholder')}
                   required
-                  type="search"
+                  type="text"
                 />
                 <button
                   className="absolute end-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-700 dark:text-gray-300 dark:hover:bg-blue-800 dark:focus:ring-blue-800"
-                  type="submit"
+                  onClick={createSharingLink}
+                  disabled={isButtonDisabled}
                 >
                   {t('create')}
                 </button>
